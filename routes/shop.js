a82ef145ccd66c4d6a34c1f16ceab36ab90f3e10
William Dafeldecker <william.dafeldecker@judiciary.state.nj.us>
2019-07-05 15:16:15 -0400

    first commit

---
+++ routes/shop.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+// : is how express checks for parameters, so it will know that param is productId, for use elsewhere
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
